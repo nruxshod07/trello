b715eb135f535f4a840c8a6b3edf820e465df131
Developer <112705739+Farrux73982011@users.noreply.github.com>
2023-10-26 20:21:34 +0500

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 let closeButton = document.querySelector('.hide_aside button img');
 let openButton = document.querySelector('.show_aside');
 let aside = document.querySelector('aside');
+let modal = document.querySelector('.modal')
+let btns_open = document.querySelectorAll('[data-modal]')
+let close_btns = document.querySelectorAll('[data-close]')
+let body = document.querySelector('body')
+let create_btn = document.querySelector('.create_btn')
+let form =document.forms.form
+let inps = document.querySelectorAll('.modal__input')
 
 function closeAside() {
   aside.style.transition = 'transform 0.3s';
@@ -16,3 +23,55 @@ function openAside() {
 
 closeButton.onclick = closeAside
 openButton.onclick = openAside
+function modalToggler(arr , callBack){
+  arr.forEach(btn =>{
+      btn.onclick = () => {
+          callBack()
+      }
+  })
+}
+form.onsubmit = (e) => {
+  e.preventDefault()
+
+  let task = {
+      
+  }
+
+  let fm = new FormData(form)
+
+  fm.forEach((value, key) => {
+    task[key] = value
+  })
+
+  console.log(task);
+  let error = false
+
+  inps.forEach(inp => {
+      if (inp.value.length === 0) {
+          inp.classList.add("error");
+      } else {
+          error = true
+          inp.classList.remove("error");
+      }
+  });
+
+  if (error) {
+      postData('/task', task)
+          .then(res => {
+              if(res.status === 200 || res.status === 201) {
+                  location.assign('/pages/cards/')
+              }
+          })
+  }
+}
+function openModal() {
+  modal.classList.add('fade', 'show')
+  modal.classList.remove('hide')
+  body.style.overflow = 'hidden'
+}
+function closeModal() {
+  modal.classList.remove('fade', 'show')
+  body.classList.remove('over')
+}
+modalToggler(btns_open, openModal)
+modalToggler(close_btns, closeModal)
