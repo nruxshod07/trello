356b270c8c6c03d3cc387836b506302017675647
Developer <112705739+Farrux73982011@users.noreply.github.com>
2023-11-03 18:09:23 +0500

    site ready

--- main.js
+++ main.js
@@ -75,3 +75,191 @@ function closeModal() {
 }
 modalToggler(btns_open, openModal)
 modalToggler(close_btns, closeModal)
+
+let add1 = document.querySelector('.add1')
+let add2 = document.querySelector('.add2')
+let add3 = document.querySelector('.add3')
+let add_div1 = document.querySelector('.add_div1')
+let add_div2 = document.querySelector('.add_div2')
+let add_div3 = document.querySelector('.add_div3')
+let close1 = document.querySelector('.close1')
+let close2 = document.querySelector('.close2')
+let close3 = document.querySelector('.close3')
+
+add1.onclick = () => {
+  add_div1.classList.add('fade', 'show')
+  add_div1.classList.remove('hide')
+}
+add2.onclick = () => {
+  add_div2.classList.add('fade', 'show')
+  add_div2.classList.remove('hide')
+}
+add3.onclick = () => {
+  add_div3.classList.add('fade', 'show')
+  add_div3.classList.remove('hide')
+}
+
+close1.onclick = () => {
+  add_div1.classList.remove('fade', 'show')
+  add_div1.classList.add('hide')
+}
+close2.onclick = () => {
+  add_div2.classList.remove('fade', 'show')
+  add_div2.classList.add('hide')
+}
+close3.onclick = () => {
+  add_div3.classList.remove('fade', 'show')
+  add_div3.classList.add('hide')
+}
+
+
+
+
+const empties = document.querySelectorAll(".empty");
+// let form = document.forms.add_task1
+// let form2 = document.forms.add_task2
+// let form3 = document.forms.add_task3
+let inp = document.querySelector(".titel");
+let des = document.querySelector(".des");
+let todos = [
+  {
+    id: "1sdffdfwe2543241",
+    description: "create trello",
+    status: "todo",
+  },
+  {
+    id: "1sadasd2543241",
+    description: "buy milk",
+    status: "inprogress",
+  },
+  {
+    id: "1sdasdasd241",
+    description: "buy fruits",
+    status: "done",
+  },
+];
+
+function dragStart() {
+  temp_id = this.id;
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function dragEnter(event) {
+  event.preventDefault();
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  this.className = "empty";
+}
+
+let temp = [];
+let temp_id;
+  for (let todo of todos) {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+  
+    div.setAttribute("id", todo.id);
+    div.setAttribute("class", "fill");
+    div.setAttribute("draggable", true);
+  
+    p.innerHTML = todo.description;
+  
+    div.append(p);
+  
+    switch (todo.status) {
+      case "todo":
+        empties[0].append(div);
+        break;
+      case "inprogress":
+        empties[1].append(div);
+        break;
+      case "done":
+        empties[2].append(div);
+        break;
+    }
+  
+    div.ondragstart = dragStart;
+    div.ondragend = dragEnd;
+  
+    temp.push(div);
+  }
+for (let empty of empties) {
+  empty.ondragover = dragOver;
+  empty.ondragenter = dragEnter;
+  empty.ondragleave = dragLeave;
+  empty.ondrop = dragDrop;
+}
+
+function dragDrop() {
+  this.className = "empty";
+  temp.forEach((item, index) => {
+    if (item.id === temp_id) {
+      this.append(item);
+    }
+  });
+}
+
+// form.onsubmit = (event) => {
+//   event.preventDefault();
+
+//   let todo = {
+//     id: Math.random(),
+//     status: document.querySelector("select").value,
+//     title: inp.value,
+//     description: des.value,
+//   };
+
+//   let fm = new FormData(event.target);
+
+//   fm.forEach((value, key) => {
+//     todo[key] = value;
+//   });
+
+//   todos.push(todo);
+// };
+// form2.onsubmit = (event) => {
+//   event.preventDefault();
+
+//   let todo = {
+//     id: Math.random(),
+//     status: document.querySelector("select").value,
+//     title: inp.value,
+//     description: des.value,
+//   };
+
+//   let fm = new FormData(event.target);
+
+//   fm.forEach((value, key) => {
+//     todo[key] = value;
+//   });
+
+//   todos.push(todo);
+// };
+// form3.onsubmit = (event) => {
+//   event.preventDefault();
+
+//   let todo = {
+//     id: Math.random(),
+//     status: document.querySelector("select").value,
+//     title: inp.value,
+//     description: des.value,
+//   };
+
+//   let fm = new FormData(event.target);
+
+//   fm.forEach((value, key) => {
+//     todo[key] = value;
+//   });
+
+//   todos.push(todo);
+// };
