446cb9946386273757616cae1151c3ecd54c6d32
Daler <dsarifkulo021@gmail.com>
2023-11-08 18:48:21 +0500

    fixed

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import {
 	trash
 } from "./modules/dragNdrop"
 import {
-	getData, postData,removeData
+	getData
 } from "./modules/http";
 import { reload } from "./modules/ui";
 
@@ -28,11 +28,11 @@ for (let empty of empties) {
 
 trash.ondragover = (e) =>{ 
 	dragOver(e)
-	trash_head.style.transform = "translateX(1%)"
+	trash_head.style.transform = "rotate(80deg)"
 };
 trash.ondragenter = dragEnterTrash;
+
 trash.ondragleave = (e) =>{ 
-	// dragLeaveTrash(e)
 	trash_head.style.transform = "translateX(0%)"
 };
 trash.ondrop = function () {
@@ -41,46 +41,3 @@ trash.ondrop = function () {
 function dragEnterTrash(event) {
 	event.preventDefault();
 }
-
-// function dragLeaveTrash() {
-// 	this.className = "trash";
-// }
-
-// getData('/tasks')
-// 	.then(res => {
-// 		let filtered = res.filter(item => {
-// 			if (item.status==null) {
-// 				console.log("/tasks"+`/${item.id}`);
-// 				removeData("/tasks",`${item.id}`)
-// 			}
-// 		})
-// 		console.log(filtered);
-// 	})
-// trash.ondragover = () =>{
-// 	dragOver
-// 	console.log(1);
-// 	trash_head.style.transform = "translateX(70%)"
-// } ;;
-// trash.ondragenter = dragEnter
-// trash.ondragleave = () =>{
-// 	dragLeave
-// 	trash_head.style.transform = "translateX(0%)"
-// } ;
-// trash.className = "trash"
-// trash.ondrop = ()=> {
-// 	console.log("ssss");
-// 	let temp = Array.from(document.querySelectorAll('.trash div'))
-// 	this.className = "trash";
-
-// 	temp.forEach((item) => {
-// 		if (item.id === temp_id) {
-// 			this.append(item);
-// 			patchData('/tasks/' + temp_id, {status: this.getAttribute('data-status')})
-// 		}
-// 	});
-// };
-// getData('/tasks')
-// 	.then(res => {
-// 		let filtered = res.filter(item => typeof(item)!==null)
-// 		console.log(filtered);
-// 	})
