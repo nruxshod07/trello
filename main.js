8328e88aec7ca064b7e3c16a218d2b15e762c514
Daler <dsarifkulo021@gmail.com>
2023-11-08 19:39:22 +0500

    fixed

--- main.js
+++ main.js
@@ -9,12 +9,21 @@ import {
 import {
 	getData
 } from "./modules/http";
-import { reload } from "./modules/ui";
+import {
+	reload
+} from "./modules/ui";
 
 const empties = document.querySelectorAll(".empty");
+const neag = document.querySelector(".neag");
+const inp = document.querySelector('#search')
+const inp_real = inp.querySelector('input')
+let tasks = []
 
 getData('/tasks')
-	.then(res => reload(res, empties))
+	.then(res => {
+		reload(res, empties)
+		tasks = res
+	})
 
 
 for (let empty of empties) {
@@ -38,6 +47,44 @@ trash.ondragleave = (e) =>{
 trash.ondrop = function () {
 	dragDrop(this)
 };
+
 function dragEnterTrash(event) {
 	event.preventDefault();
+}
+
+inp.onclick = () => {
+	getData('/tasks')
+		.then(res => tasks = res)
+	neag.style.display = 'block'
+	inp.style.width = '470px'
+	inp.style.boxShadow = '2px 2px 2px 2px rgb(165, 165, 165)'
+	neag.onclick = () => {
+		neag.style.display = 'none'
+		inp.style.width = '200px'
+		inp.style.boxShadow = "none"
+	}
+}
+
+inp_real.onkeyup = () => {
+	let val = inp_real.value.toLowerCase().trim()
+
+	if(val.length === 0) return
+
+	let filtered = tasks.filter(item => {
+		let title = item.title.toLowerCase().trim()
+		if(title.includes(val)) {
+			return item
+		}
+	})
+	
+	let temp = Array.from(document.querySelectorAll('.empty div'))
+
+	for(let div of temp) {
+		for(let task of tasks) {
+			if(div.id.toString() === task.id.toString()) {
+				div.classList.add('finded')
+			}
+		}
+	}
+	
 }
