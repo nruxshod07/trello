34303aa332933f2dc317b9b329c75f27b891bdc5
RT <gulsanambegmatova@gmail.com>
2023-11-10 13:45:03 +0500

    search

--- main.js
+++ main.js
@@ -9,12 +9,21 @@ import {
 import {
 	getData
 } from "./modules/http";
-import { reload } from "./modules/ui";
+import {
+	reload
+} from "./modules/ui";
 
 const empties = document.querySelectorAll(".empty");
+const neag = document.querySelector(".neag");
+const inp = document.querySelector('#search')
+const inp_real = inp.querySelector('input')
+let tasks = []
 
 getData('/tasks')
-	.then(res => reload(res, empties))
+	.then(res => {
+		reload(res, empties)
+		tasks = res
+	})
 
 
 for (let empty of empties) {
@@ -26,18 +35,71 @@ for (let empty of empties) {
 	};
 }
 
-trash.ondragover = (e) =>{ 
+trash.ondragover = (e) => {
 	dragOver(e)
 	trash_head.style.transform = "rotate(80deg)"
 };
 trash.ondragenter = dragEnterTrash;
 
-trash.ondragleave = (e) =>{ 
+trash.ondragleave = (e) => {
 	trash_head.style.transform = "translateX(0%)"
 };
 trash.ondrop = function () {
 	dragDrop(this)
 };
+
 function dragEnterTrash(event) {
 	event.preventDefault();
+	trash_head.style.marginLeft = "50px"
+}
+inp_real.onfocus = () => {
+	getData('/tasks')
+		.then(res => tasks = res)
+	neag.style.display = 'block'
+	inp.style.width = '400px'
+	inp.classList.add("boxShadow")
+	inp.style.zindex = "11"
+}
+inp_real.onblur = () => {
+	neag.style.display = 'none'
+	inp.style.width = '200px'
+	inp.classList.remove("boxShadow")
+	let temp = Array.from(document.querySelectorAll('.empty div'))
+
+	for (let div of temp) {
+		div.classList.remove("boxShadow")
+		div.classList.remove('finded')
+	}
+}
+
+
+inp_real.onkeyup = () => {
+	let val = inp_real.value.toLowerCase().trim()
+	
+	if (val.length === 0) return
+
+	let filtered = tasks.filter(item => {
+		let title = item.title.toLowerCase().trim()
+		if (title.includes(val)) {
+			
+			return item
+		}
+	})
+	let temp = Array.from(document.querySelectorAll('.empty div'))
+
+	for (let div of temp) {
+
+		for (let task of filtered) {
+			if (div.id.toString() === task.id.toString()) {
+				div.classList.add("boxShadow")
+				div.classList.add('finded')
+			}else{
+				div.classList.remove('finded')
+				div.classList.remove("boxShadow")
+
+			}
+		}
+	}
+
+
 }
